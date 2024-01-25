2cad5e95898369f5ad7f14926ef5777d349346fb
prehmieren <prehm.soongnart@hotmail.com>
2024-01-25 22:44:53 +0700

    2nd cm

---
+++ src/assets/studentM4.js
@@ -0,0 +1,52 @@
+export const m4_data =
+    [
+        {
+            "id": 1,
+            "name": "Dino",
+            "img": "./src/stundentPics/M4/Dino.png"
+        },
+        {
+            "id": 2,
+            "name": "Fabian",
+            "img": "./src/stundentPics/M4/Fabian.png"
+        },
+        {
+            "id": 3,
+            "name": "Karim",
+            "img": "./src/stundentPics/M4/Karim.png"
+        }
+        ,
+        {
+            "id": 4,
+            "name": "Melwin",
+            "img": "./src/stundentPics/M4/Melwin.png"
+        }
+        ,
+        {
+            "id": 5,
+            "name": "Nathan",
+            "img": "./src/stundentPics/M4/Nathan.png"
+        }
+        ,
+        {
+            "id": 6,
+            "name": "Nina",
+            "img": "./src/stundentPics/M4/Nina.png"
+        }
+        ,
+        {
+            "id": 7,
+            "name": "Patrick",
+            "img": "./src/stundentPics/M4/Patrick.png"
+        },
+        {
+            "id": 8,
+            "name": "Paul",
+            "img": "./src/stundentPics/M4/Paul.png"
+        },
+        {
+            "id": 9,
+            "name": "Shawn",
+            "img": "./src/stundentPics/M4/Shawn.png"
+        }
+    ]
---
+++ src/assets/studentM5.js
@@ -0,0 +1,52 @@
+export const m5_data =
+    [
+        {
+            "id": 1,
+            "name": "Claudia",
+            "img": "./src/stundentPics/M5/Claudia.png"
+        },
+        {
+            "id": 2,
+            "name": "Florian",
+            "img": "./src/stundentPics/M5/Florian.png"
+        },
+        {
+            "id": 3,
+            "name": "Freya",
+            "img": "./src/stundentPics/M5/Freya.png"
+        }
+        ,
+        {
+            "id": 4,
+            "name": "Ingrid",
+            "img": "./src/stundentPics/M5/Ingrid.png"
+        }
+        ,
+        {
+            "id": 5,
+            "name": "Kerstin",
+            "img": "./src/stundentPics/M5/Kerstin.png"
+        }
+        ,
+        {
+            "id": 6,
+            "name": "Mathilda",
+            "img": "./src/stundentPics/M5/Mathilda.png"
+        }
+        ,
+        {
+            "id": 7,
+            "name": "Mia",
+            "img": "./src/stundentPics/M5/Mia.png"
+        },
+        {
+            "id": 8,
+            "name": "Sandra",
+            "img": "./src/stundentPics/M5/Sandra.png"
+        },
+        {
+            "id": 9,
+            "name": "Stephanie",
+            "img": "./src/stundentPics/M5/Stephanie.png"
+        }
+    ]
